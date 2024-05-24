7fa219b8a6397915e725c8fb40920d5cff2d25bb
Caleb <souzacaleb@gmail.comm>
2024-05-23 22:38:55 -0300

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\caleb.souza\downloads\drf_escola_api-aula_4\drf_escola_api-aula_4\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
